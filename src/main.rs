871253e1f09b7af178dee67b6f51fb24d0820799
Connor Mooneyhan <connmoo@gmail.com>
2021-12-05 20:13:42 -0500

    Add basic game initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+/// State machine providing the following three game modes:
+/// * Menu
+/// * Playing
+/// * End
+enum GameMode {
+    Menu,
+    Playing,
+    End
+}
+
+/// Game state struct equipped with `tick` method for updating state
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            mode: GameMode::Menu
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
+    // Configures display window
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    // Starts game loop; returns BError
+    main_loop(context, State::new())
 }
